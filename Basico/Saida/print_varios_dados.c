4c8705078bdbe604a44a7da6484663546183280b
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-13 00:50:23 +0000

    Estuda print com varios tipos de saida

--- Basico/Saida/print_varios_dados.c
+++ Basico/Saida/print_varios_dados.c
@@ -4,7 +4,7 @@ int main() {
     int num1, num2, num3;
     printf("Digite tres calores inteiros: ");
     scanf("%d%d%d", &num1, &num2,&num3);
-    printf("Vlores lidos: %d, %d e %d\n", num1, num2, num3);
+    printf("Valores lidos: %d, %d e %d\n", num1, num2, num3);
     
     return 0;
 }
