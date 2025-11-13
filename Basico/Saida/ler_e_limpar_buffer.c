9bb040b3bc5554f515de2a9caabb8225a4deaf6a
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-13 01:21:09 +0000

    Coriige o buffer de memoria em string

---
+++ Basico/Saida/ler_e_limpar_buffer.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (){
+
+    char a, b;
+
+    printf("Digite duas letras: ");
+    scanf("%c %c", &a, &b); // se, espaço entre %c o enter vai pro buffer como \n
+    printf("Primeira letra: %c\nSegunda letra: %c\n", a, b);
+    //fflush(); não é recomendado para limpeza, só funciona no windows
+    return 0;
+}
