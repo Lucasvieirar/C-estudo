b4b8783eeeb2a31baa6b41937957a91cc5889c7f
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-13 01:03:47 +0000

    Conserta o buffer de memoria

--- Basico/Saida/print_varios_tipos.c
+++ Basico/Saida/print_varios_tipos.c
@@ -7,8 +7,9 @@ int main (){
     int idade;
     float peso, altura;
     printf("Digite sexo (f, F, m ou M), idade, peso e altura:\n");
-    scanf("%c%d%f%f", &sexo, &idade, &peso, &altura);
-
+    scanf("%c %d %f %f", &sexo, &idade, &peso, &altura);
+    //buffer do teclado-> armazena o que o usuario digitou 
+    // adiciona espaço entre caracteres parfa limpar o buffer
     printf("Sexo: %c\nIdade: %d\nPeso: %.1f\nAltura: %2.f\n", sexo, idade, peso, altura);
     return 0;
 }
