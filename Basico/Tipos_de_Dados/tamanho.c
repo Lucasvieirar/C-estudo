79933b7ab2aac5d521cdf578e5f763da2e9d754d
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2025-11-20 01:08:33 +0000

    sizeof é uma constante

--- Basico/Tipos_de_Dados/tamanho.c
+++ Basico/Tipos_de_Dados/tamanho.c
@@ -3,7 +3,7 @@
 
 int main(){
     float x = 1.0;
-    
+    //sizeof é um constante
     printf("Tamanho de um int na menoria: %d bytes\n", sizeof x ); // variavel atribuida não é obrigado o uso de parentese
     printf("Tamanho de menoria de int: %d bytes \n", sizeof(int)); //saber o tamanho do meu tipo
     // 4 * 8 = 32
