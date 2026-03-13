91f06694f670839483e6ec6294dcc2de8e7302dc
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-13 21:11:51 +0000

    Resto da divisão

---
+++ Basico/Operadores_matematicos/resto_da_divisao.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/* 
+    Resto da divisão
+
+*/
+int main() {
+    int a = 10, b = 20;
+
+    printf("\n Resto da divisao: %d\n", a % b);
+    printf("\n Para saber se e par: %d\n", a % 2);
+   
+   
+   return 0;
+}
