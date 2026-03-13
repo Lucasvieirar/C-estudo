12df9afd092d07d71744cdcc16c57b78fc894794
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-13 19:42:37 +0000

    operadores matematicos

---
+++ Basico/Operadores_matematicos/operadores.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/* 
+    Operadores Matemáticos:
+    Soma +
+    Subtração -
+    Multiplicação *
+    Divisão /
+
+*/
+int main() {
+    printf("\n Soma: %d\n", 10 + 20);
+    printf("\n Subtracao: %d\n", 10 - 20);
+    printf("\n Multiplicação: %d\n", 10 * 20);
+    printf("\n Divisao: %.2f\n",  10.0 / 20);
+   
+   return 0;
+}
