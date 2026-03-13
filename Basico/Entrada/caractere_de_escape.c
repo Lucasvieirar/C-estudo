d56c1010846bb908d81377d90a2e48b7bd710f57
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-13 19:29:55 +0000

    caracteres de escape

---
+++ Basico/Entrada/caractere_de_escape.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/* 
+    Caracteres de escape
+    Quebra de linha -> \n
+    Tabulação  -> \t
+    Aspa dubla -> \"
+    Aspa simples -> \'
+    Contrabarra -> \\
+
+*/
+int main() {
+    
+   printf("\n\n\t Isto e uma mensagem \"a\" \\ ser impresa na tela.\n\n");
+   
+   return 0;
+}
