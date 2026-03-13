15e2272bdecef096caad70c3681a92d3f1c3aef6
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-13 21:51:01 +0000

    incremento

---
+++ Basico/incremento e decremento/incremento.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/* 
+    Resto da divisão
+
+*/
+int main() {
+    int resultado, contador = 10, contador2=10;
+    
+    printf("\nValor: %d\n", ++contador);
+    
+    printf("\nValor: %d\n", contador2++);
+    
+    printf("\nValor: %d\n", contador2);
+    // contador ++;
+    // contador += 1;
+    // contador = contador + 1;
+   
+   
+   return 0;
+}
