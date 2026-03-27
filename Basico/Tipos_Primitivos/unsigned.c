4c364494c1183e07e2bbd5bc1e05a1e084be7e29
Lucasvieirar <lucasvieiraribeiro2055@gmail.com>
2026-03-27 22:16:30 +0000

    operador unsigned

---
+++ Basico/Tipos_Primitivos/unsigned.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/* 
+    Operador unsigned
+    trcar o %d por %u
+    limite para o tipo int: 2.147.483.647
+    unisigned 4.294.967.295
+*/
+int main() {
+    unsigned  int x = 2147483647; /// tira a parte negativa
+    // 31 numero
+    // 32-> sinal do numero 
+    
+    printf("\n\t%u\n\n", ++x);
+
+
+   
+   
+   return 0;
+}
